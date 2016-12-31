1264dc3049eb52adf1ed3e5ca6605c7e05358a6a
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2016-12-31 19:12:38 +0100

    send coverage #57

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,4 +1,6 @@
 #gulp test # gulp test blocks on finish
 gulp test
 
-codeclimate-test-reporter < coverage/lcov.info
+# TODO FIXME it's working, but I commented this line because I have to fix a bug related to integration testing with gulp
+# see issue
+#codeclimate-test-reporter < coverage/lcov.info
