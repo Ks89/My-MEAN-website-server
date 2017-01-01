0ec14ef949ea42d38c6a865a4d316a854b217d51
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-01 21:30:06 +0100

    restored codeclimate's command to send coverage report on travisci

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,6 +1,3 @@
-#gulp test # gulp test blocks on finish
 gulp test
 
-# TODO FIXME it's working, but I commented this line because I have to fix a bug related to integration testing with gulp
-# see issue
-#codeclimate-test-reporter < coverage/lcov.info
+codeclimate-test-reporter < coverage/lcov.info
