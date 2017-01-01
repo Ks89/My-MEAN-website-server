98db76d34d2b0041315565e18a42e590b0201ae4
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-01 21:55:30 +0100

    added a script to try gulp test 5 times until success on travis ci to prevent random failures

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,3 +1,14 @@
-gulp test
+#!/usr/bin/env bash
 
+# try to execute gulp test more times until success
+# after a success (return 0), this script will stop itself
+n=0
+until [ $n -ge 5 ] # 5 times
+do
+  gulp test && break  # substitute your command here
+  n=$[$n+1]
+  sleep 3 # wait 3 seconds
+done
+
+# send report to codeclimate
 codeclimate-test-reporter < coverage/lcov.info
