3616671ecb0b96876e93c83fc149080f8b576d5c
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-02 19:28:55 +0100

    refactoring to simplify process.env.CI login #60[A

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # run gulp test
-# try to execute npm test more times until success
+# try to execute gulp test more times until success
 # after a success (return 0), this script will stop itself
-echo "npm test on $TRAVIS_OS_NAME ready to start"
+echo "gulp test on $TRAVIS_OS_NAME ready to start"
 n=0
 until [ $n -ge 5 ] # 5 times
 do
-  echo "npm test on $TRAVIS_OS_NAME with n=$n"
-  npm test && break
+  echo "gulp test on $TRAVIS_OS_NAME with n=$n"
+  gulp test && break
   n=$[$n+1]
   sleep 3 # wait 3 seconds
 done
-echo "npm test on $TRAVIS_OS_NAME finished with n=$n"
+echo "gulp test on $TRAVIS_OS_NAME finished with n=$n"
 
 # send test coverage to codeclimate.com
 echo "npm run codeclimate on $TRAVIS_OS_NAME"
