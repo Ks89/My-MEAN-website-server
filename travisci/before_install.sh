9caaa85f2bff3ef9dcd414e9dc2a43d5036cf92b
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-02 17:13:18 +0100

    add osx environment to travisci (server-side) #60

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -1,3 +1,48 @@
+#!/usr/bin/env bash
+
+echo "Before install - OS is $TRAVIS_OS_NAME"
+
+echo "Exporting env variables dependencies"
+# export env variables, thanks to https://github.com/travis-ci/travis-ci/issues/7099
+#if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+# in this project, all env variables are the same for both linux and osx
+echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
+
+export NODE_ENV=test CI=yes JWT_SECRET=faketestjwt TWITTER_CONSUMER_KEY=consumerkey TWITTER_CONSUMER_SECRET=consumersecret TWITTER_CALLBACK_URL=http://127.0.0.1:3300/api/auth/twitter/callback FACEBOOK_APP_ID=appid FACEBOOK_APP_SECRET=appsecret FACEBOOK_CALLBACK_URL=http://localhost:3300/api/auth/facebook/callback GOOGLE_CLIENT_ID=clientid GOOGLE_CLIENT_SECRET=clientsecret GOOGLE_CALLBACK_URL=http://localhost:3300/api/auth/google/callback GITHUB_CLIENT_ID=clientid GITHUB_CLIENT_SECRET=clientsecret GITHUB_CALLBACK_URL=http://localhost:3300/api/auth/github/callback LINKEDIN_CLIENT_ID=clientid LINKEDIN_CLIENT_SECRET=clientsecret LINKEDIN_CALLBACK_URL=http://localhost:3300/api/auth/linkedin/callback USER_EMAIL=fake@fake.it PASS_EMAIL=fakepasswordemail RECAPTCHA_PUBLIC=recaptchapublic RECAPTCHA_SECRET=recaptchasecret
+
+echo "NODE_ENV = $NODE_ENV"
+echo "CI = $CI"
+echo "JWT_SECRET = $JWT_SECRET"
+echo "TWITTER_CONSUMER_KEY = $TWITTER_CONSUMER_KEY"
+echo "TWITTER_CONSUMER_SECRET = $TWITTER_CONSUMER_SECRET"
+echo "TWITTER_CALLBACK_URL = $TWITTER_CALLBACK_URL"
+echo "FACEBOOK_APP_ID = $FACEBOOK_APP_ID"
+echo "FACEBOOK_APP_SECRET = $FACEBOOK_APP_SECRET"
+echo "FACEBOOK_CALLBACK_URL = $FACEBOOK_CALLBACK_URL"
+echo "GOOGLE_CLIENT_ID = $GOOGLE_CLIENT_ID"
+echo "GOOGLE_CLIENT_SECRET = $GOOGLE_CLIENT_SECRET"
+echo "GOOGLE_CALLBACK_URL = $GOOGLE_CALLBACK_URL"
+echo "GITHUB_CLIENT_ID = $GITHUB_CLIENT_ID"
+echo "GITHUB_CLIENT_SECRET = $GITHUB_CLIENT_SECRET"
+echo "GITHUB_CALLBACK_URL = $GITHUB_CALLBACK_URL"
+echo "LINKEDIN_CLIENT_ID = $LINKEDIN_CLIENT_ID"
+echo "LINKEDIN_CLIENT_SECRET = $LINKEDIN_CLIENT_SECRET"
+echo "LINKEDIN_CALLBACK_URL = $LINKEDIN_CALLBACK_URL"
+echo "USER_EMAIL = $USER_EMAIL"
+echo "PASS_EMAIL = $PASS_EMAIL"
+echo "RECAPTCHA_PUBLIC = $RECAPTCHA_PUBLIC"
+echo "RECAPTCHA_SECRET = $RECAPTCHA_SECRET"
+
+echo "Exporting env variables - done"
+#else
+   # echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
+#fi
+
+
+echo "Installing global dependencies"
+# install global dependencies
+
+echo "Installing $TRAVIS_OS_NAME global dependencies"
 npm install -g mocha
 npm install -g nodemon
 npm install -g gulp@github:gulpjs/gulp#4.0
--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+# run npm test
 # try to execute gulp test more times until success
 # after a success (return 0), this script will stop itself
+echo "npm run test on $TRAVIS_OS_NAME ready to start"
 n=0
 until [ $n -ge 5 ] # 5 times
 do
-  gulp test && break
+  echo "npm run test on $TRAVIS_OS_NAME with n=$n"
+  npm run test && break
   n=$[$n+1]
   sleep 3 # wait 3 seconds
 done
+echo "npm run test on $TRAVIS_OS_NAME finished with n=$n"
 
 # send test coverage to codeclimate.com
-codeclimate-test-reporter < coverage/lcov.info
+echo "npm run codeclimate on $TRAVIS_OS_NAME"
+npm run codeclimate
 # send test coverage to coveralls.io
+echo "npm run coveralls on $TRAVIS_OS_NAME"
 npm run coveralls
