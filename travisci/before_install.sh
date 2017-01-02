d7eaa3c12ec3fa06ebb0b1d1ecc6be76228cc95f
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-02 18:35:06 +0100

    refactoring to simplify process.env.CI login #60

--- travisci/before_install.sh
+++ travisci/before_install.sh
@@ -8,7 +8,28 @@ echo "Exporting env variables dependencies"
 # in this project, all env variables are the same for both linux and osx
 echo "Exporting env variables - OS is $TRAVIS_OS_NAME"
 
-export NODE_ENV=test CI=yes JWT_SECRET=faketestjwt TWITTER_CONSUMER_KEY=consumerkey TWITTER_CONSUMER_SECRET=consumersecret TWITTER_CALLBACK_URL=http://127.0.0.1:3300/api/auth/twitter/callback FACEBOOK_APP_ID=appid FACEBOOK_APP_SECRET=appsecret FACEBOOK_CALLBACK_URL=http://localhost:3300/api/auth/facebook/callback GOOGLE_CLIENT_ID=clientid GOOGLE_CLIENT_SECRET=clientsecret GOOGLE_CALLBACK_URL=http://localhost:3300/api/auth/google/callback GITHUB_CLIENT_ID=clientid GITHUB_CLIENT_SECRET=clientsecret GITHUB_CALLBACK_URL=http://localhost:3300/api/auth/github/callback LINKEDIN_CLIENT_ID=clientid LINKEDIN_CLIENT_SECRET=clientsecret LINKEDIN_CALLBACK_URL=http://localhost:3300/api/auth/linkedin/callback USER_EMAIL=fake@fake.it PASS_EMAIL=fakepasswordemail RECAPTCHA_PUBLIC=recaptchapublic RECAPTCHA_SECRET=recaptchasecret
+export NODE_ENV=test;
+export CI=yes;
+export JWT_SECRET=faketestjwt;
+export TWITTER_CONSUMER_KEY=consumerkey;
+export TWITTER_CONSUMER_SECRET=consumersecret;
+export TWITTER_CALLBACK_URL=http://127.0.0.1:3300/api/auth/twitter/callback;
+export FACEBOOK_APP_ID=appid;
+export FACEBOOK_APP_SECRET=appsecret;
+export FACEBOOK_CALLBACK_URL=http://localhost:3300/api/auth/facebook/callback;
+export GOOGLE_CLIENT_ID=clientid;
+export GOOGLE_CLIENT_SECRET=clientsecret;
+export GOOGLE_CALLBACK_URL=http://localhost:3300/api/auth/google/callback;
+export GITHUB_CLIENT_ID=clientid;
+export GITHUB_CLIENT_SECRET=clientsecret;
+export GITHUB_CALLBACK_URL=http://localhost:3300/api/auth/github/callback;
+export LINKEDIN_CLIENT_ID=clientid;
+export LINKEDIN_CLIENT_SECRET=clientsecret;
+export LINKEDIN_CALLBACK_URL=http://localhost:3300/api/auth/linkedin/callback;
+export USER_EMAIL=fake@fake.it;
+export PASS_EMAIL=fakepasswordemail;
+export RECAPTCHA_PUBLIC=recaptchapublic;
+export RECAPTCHA_SECRET=recaptchasecret;
 
 echo "NODE_ENV = $NODE_ENV"
 echo "CI = $CI"
