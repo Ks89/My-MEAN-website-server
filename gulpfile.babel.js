2d8bb8e2b9cae83aa1e061f576058a6dfeeebb28
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-04-09 17:27:15 +0200

    jshint improvements and added a new script to run jshint via CLI #76

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,7 @@
 
 import gulp         from 'gulp';
 import del          from 'del';
-import jshint       from 'gulp-jshint';
+// import jshint       from 'gulp-jshint';
 import mocha        from 'gulp-mocha';
 import istanbul     from 'gulp-istanbul';
 import nodemon      from 'gulp-nodemon';
@@ -10,6 +10,7 @@ import sourcemaps   from 'gulp-sourcemaps';
 import through      from 'through2';
 import yargs        from 'yargs';
 import exit         from 'gulp-exit';
+import babel        from 'gulp-babel';
 
 const args = yargs.argv;
 const isprod = (args.env === 'production');
@@ -26,7 +27,7 @@ const prod = task => isprod ? task : noop();
 // *                   FILE PATHS                    *
 // ***************************************************
 const filePaths = ['src/**/*.js', './app.js'];
-const testHintJs = ['src/**/*.js', './app.js'];
+// const testHintJs = ['src/**/*.js', './app.js'];
 const testPaths = [
   'test-server-unit/util.spec.js',
   'test-server-integration/**/*.spec.js',
@@ -43,19 +44,19 @@ const coveragePaths = ['coverage'];
 // *                     JSHINT                      *
 // ***************************************************
 // call with `./node_modules/.bin/gulp --gulpfile gulpfile.babel.js hint`
-export function hint() {
-  return gulp.src(testHintJs/*, {since: gulp.lastRun(hint)}*/)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(jshint.reporter('fail'));
-}
+// export function hint() {
+//   return gulp.src(testHintJs/*, {since: gulp.lastRun(hint)}*/)
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('default'))
+//     .pipe(jshint.reporter('fail'));
+// }
 
 // ***************************************************
 // *                      TEST                       *
 // ***************************************************
 // call with `./node_modules/.bin/gulp --gulpfile gulpfile.babel.js test`
 const test = gulp.series(cleanCoverage, preTest, testInternal);
-export { test };
+export {test};
 
 function cleanCoverage() {
   return del(coveragePaths);
@@ -63,19 +64,17 @@ function cleanCoverage() {
 
 function preTest() {
   return gulp.src(sourcemapPaths)
-  // optionally load existing source maps
-    .pipe(sourcemaps.init())
-    // Covering files
-    .pipe(istanbul())
-    // Force `require` to return covered files
-    .pipe(istanbul.hookRequire());
+    .pipe(sourcemaps.init()) // optionally load existing source maps
+    .pipe(istanbul()) // Covering files
+    .pipe(istanbul.hookRequire()); // Force `require` to return covered files
 }
 
 function testInternal() {
   return gulp.src(testPaths)
     .pipe(mocha({
       timeout: 10000,
-      useColors: true
+      useColors: true,
+      compilers: 'js:babel-core/register'
     }))
     // .pipe(mocha().on("error", (err) => { this.emit('end'); }))
     // Creating the reports after tests ran
@@ -87,9 +86,9 @@ function testInternal() {
     // to force gulp to exit (because my integration testing aren't closing connections / server)
     .pipe(exit());
 
-    // Enforce a coverage of at least 90% otherwise throw an error
-    // FIXME this throws an error. I don't known why. Probably it's a gulp's bug
-    // .pipe(istanbul.enforceThresholds({ thresholds: { global: 80,  each: 85 } }));
+  // Enforce a coverage of at least 90% otherwise throw an error
+  // FIXME this throws an error. I don't known why. Probably it's a gulp's bug
+  // .pipe(istanbul.enforceThresholds({ thresholds: { global: 80,  each: 85 } }));
 }
 
 // ***************************************************
@@ -101,15 +100,16 @@ export function nodemontask() {
     script: 'bin/www',
     ext: 'js',
     env: {'NODE_ENV': process.env.NODE_ENV},
-    ignore: ['test-server-integration', 'test-server-unit'],
-    tasks: ['hint'] });
+    ignore: ['test-server-integration', 'test-server-unit']
+    //tasks: ['hint']
+  });
 
   stream
     .on('restart', function () {
       console.log('restarted!');
     })
-    .on('crash', function() {
-      console.error('Application has crashed!\n')
+    .on('crash', function () {
+      console.error('Application has crashed!\n');
       stream.emit('restart', 10);  // restart the server in 10 seconds
     });
 
@@ -117,6 +117,6 @@ export function nodemontask() {
 }
 
 // call default task with `./node_modules/.bin/gulp --gulpfile gulpfile.babel.js`
-const buildDefault = gulp.series(hint, nodemontask);
+const buildDefault = gulp.series(/*hint, */nodemontask);
 export default buildDefault;
 
