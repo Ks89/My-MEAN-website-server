4bdde526cc11952c8d9fc404d0ec4717e9d3bf34
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-05-08 22:09:20 +0200

    experimenting circle ci 1.0 #83

---
+++ circleci/circleci_script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo "Travis script on CircleCI"
+
+# run npm test
+# try to execute npm test more times until success
+# after a success (return 0), this script will stop itself
+echo "npm test on CircleCI ready to start"
+n=0
+until [ $n -ge 5 ] # 5 times
+do
+  echo "npm test on CircleCI with n=$n"
+  npm test && break
+  n=$[$n+1]
+  sleep 3 # wait 3 seconds
+done
+echo "npm test on CircleCI finished with n=$n"
+
+echo "checking test threshold"
+npm run nyc:threshold
